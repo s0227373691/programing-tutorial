7b883f22230bb28f83782cbb87133891a0d4e30d
Elmer <s0227373691@gms.tku.edu.tw>
2023-08-03 17:14:08 +0800

    .

--- @daulathussain/Build Your Own Uniswap Exchange Clone/uniswapclone/src/components/Token/Token.jsx
+++ @daulathussain/Build Your Own Uniswap Exchange Clone/uniswapclone/src/components/Token/Token.jsx
@@ -35,7 +35,7 @@ const Token = ({setOpenSetting}) => {
 
             <div className={Style.Token_box_toggle}>
                 <p className={Style.Token_box_para}>Transaction deadline</p>
-                <Toggle />
+                <Toggle label="No" />
             </div>
         </div>
     </div>
